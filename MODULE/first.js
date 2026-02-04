a86904b0c35b41b19df8a7b43b387e013c17b78c
Chandan Dwivedi <chandan.dwivedi_cs24@gla.ac.in>
2026-02-04 23:21:56 +0530

    basics of module

---
+++ MODULE/first.js
@@ -0,0 +1,11 @@
+function infor(){
+    console.log("chandannn");
+}
+function info(){
+    console.log("dwivedi");
+}
+
+module.exports.infor = infor;
+module.exports = {i:infor};
+module.exports.i = info;
+ 
---
+++ MODULE/second.js
@@ -0,0 +1,3 @@
+const data = require("./first");
+data.i();
+ 
